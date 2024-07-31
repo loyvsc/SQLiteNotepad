3c809c48b0b1d24123483454113987706f557c67
Valery Varaksa <varaksav62@gmail.com>
2024-07-31 22:20:14 +0300

    Create ViewModelBase

---
+++ SQLiteNotepad.Presentation/ViewModels/ViewModelBase.cs
@@ -0,0 +1,13 @@
+using Microsoft.Extensions.Logging;
+
+namespace SQLiteNotepad.Presentation.ViewModels;
+
+public abstract class ViewModelBase : NotifyPropertyChangedBase
+{
+    protected readonly ILogger _logger;
+    
+    protected ViewModelBase(ILogger logger)
+    {
+        _logger = logger;
+    }
+}
